caaf80082e8d4311f200bf9fcac0e21ffbc57c1e
fanyanyan <fanyan@bilibili.com>
2022-01-28 17:40:25 +0800

    add

---
+++ src/components/code.vue
@@ -0,0 +1,18 @@
+// App.vue
+<template>
+  <drag-code :code="currentCode" suffix="vue"></drag-code>
+  <hg-code :code="currentCode" suffix="vue"> </hg-code>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  },
+  computed: {
+      currentCode() { 
+        return require('!!text-loader!' + '../App')
+      },
+    },
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+import App from './components/code.vue'
+import code from 'vue3-highlight-code'
+import 'vue3-highlight-code/dist/vue3-highlight-code.css'
+import 'highlight.js/styles/atom-one-dark.css'
+const app = createApp(App)
+app.use(code)
+app.mount('#app')
